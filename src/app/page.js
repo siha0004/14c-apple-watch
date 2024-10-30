02c11b1360395b75288a672bc4a514144105d8c4
Sif Melkane <siha0004@stud.kea.dk>
2024-10-30 11:31:46 +0100

    leftovers

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-cols-[minmax(1rem,_1fr)_minmax(0,_1200px)_minmax(1rem,_1fr)] grid-rows-[auto_1fr_auto] bg-gradient-to-br from-[#C8DCE5] to-[#B6CCDA] bg-no-repeat	">
-      <Header className="grid-cols-subgrid col-start-2"></Header>
+      <Header></Header>
       <main className="grid-cols-subgrid col-start-2">
         <h1 className="text-white text-7xl">
           <span className=" font-bold">The Perfect Moment</span> Between Past And Future.
