01677579bfdfff07b9cf962fd51b6413e5d7c21c
Sif Melkane <siha0004@stud.kea.dk>
2024-10-31 10:48:45 +0100

    Color indicator changing color

--- src/components/ColorDot.jsx
+++ src/components/ColorDot.jsx
@@ -1,5 +1,5 @@
 const ColorIndicator = ({ hexcolor }) => {
-  return <li className={`aspect-square w-5 bg-[${hexcolor}] bg-[#58D7C4] border-solid border-2 border-white rounded-full`}></li>;
+  return <li className={`aspect-square w-5 ${hexcolor} border-solid border-2 border-white rounded-full`}></li>;
 };
 
 export default ColorIndicator;
--- src/components/ColorIndicator.jsx
+++ src/components/ColorIndicator.jsx
@@ -4,15 +4,15 @@ import ColorDot from "@/components/ColorDot";
 const ColorIndicator = () => {
   return (
     <ul className=" text-white flex flex-col gap-1 items-center">
-      <ColorDot hexcolor="#404354"></ColorDot>
+      <ColorDot hexcolor="bg-[#404354]"></ColorDot>
       <li>
         <RiMore2Line />
       </li>
-      <ColorDot hexcolor="#58D7C4"></ColorDot>
+      <ColorDot hexcolor="bg-[#58D7C4]"></ColorDot>
       <li>
         <RiMore2Line />
       </li>
-      <ColorDot hexcolor="#F2CEC6"></ColorDot>
+      <ColorDot hexcolor="bg-[#F2CEC6]"></ColorDot>
     </ul>
   );
 };
